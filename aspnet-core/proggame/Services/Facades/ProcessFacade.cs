68584296ba01351c43a6f700e80e258decbdffa3
Hargita Benjamin <hargita.beni@gmail.com>
2023-11-27 22:29:20 +0100

    add facada skeleton

---
+++ aspnet-core/proggame/Services/Facades/ProcessFacade.cs
@@ -0,0 +1,20 @@
+﻿using Volo.Abp.DependencyInjection;
+
+namespace proggame.Services.Facades
+{
+    public class ProcessFacade : ISingletonDependency
+    {
+        public void RunProcessMultiArgs(string file, string[] args)
+        {
+
+        }
+        public void RunProcess(string file, string arg) 
+        {
+            
+        }
+        private string GetPath(string path)
+        {
+            return string.Empty;
+        }
+    }
+}
