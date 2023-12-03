13e2e5944e67184d4e09381b64a414f2e2a64c82
Kuszter <92097700+Kuszter@users.noreply.github.com>
2023-12-03 19:08:14 +0100

    Facade done

--- aspnet-core/proggame/Services/Facades/ProcessFacade.cs
+++ aspnet-core/proggame/Services/Facades/ProcessFacade.cs
@@ -1,4 +1,8 @@
-﻿using Volo.Abp.DependencyInjection;
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using Volo.Abp.DependencyInjection;
 
 namespace proggame.Services.Facades
 {
@@ -6,15 +10,41 @@ namespace proggame.Services.Facades
     {
         public void RunProcessMultiArgs(string file, string[] args)
         {
-            throw new NotImplementedException();
+            string filePath = GetPath(file);
+            if (filePath == null)
+            {
+                throw new FileNotFoundException($"The file '{file}' was not found in the PATH.");
+            }
+
+            using (Process process = new Process())
+            {
+                process.StartInfo.FileName = filePath;
+                process.StartInfo.Arguments = string.Join(" ", args);
+                process.StartInfo.CreateNoWindow = true;
+                process.Start();
+
+                process.WaitForExit();
+            }
         }
+
         public void RunProcess(string file, string arg)
         {
-            throw new NotImplementedException();
+            RunProcessMultiArgs(file, new string[] { arg });
         }
-        private string GetPath(string path)
+
+        private string GetPath(string fileName)
         {
-            throw new NotImplementedException();
+            var paths = Environment.GetEnvironmentVariable("PATH");
+            foreach (var path in paths.Split(Path.PathSeparator))
+            {
+                var fullPath = Path.Combine(path, fileName);
+                if (File.Exists(fullPath))
+                {
+                    return fullPath;
+                }
+            }
+
+            return null;
         }
     }
 }
